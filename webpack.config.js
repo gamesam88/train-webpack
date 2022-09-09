f3bea4192166dcc20280946dd2605b777ddbaf31
gamesam88 <dyxpp@rambler.com>
2022-09-09 21:55:51 +0300

    some

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
         rules: [
             { test: /\.svg$/, use: 'svg-inline-loader' },
             { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            { test: /\.(js)$/, use: 'babel-loader' }
         ]
     }
 };
